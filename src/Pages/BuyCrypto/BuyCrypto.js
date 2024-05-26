83c169a280035250eeec224cc6a149963267b9a8
Baris Ekin ISIK <06bisik03@gmail.com>
2024-05-26 22:35:31 +0300

    Functionality buy crypto done

--- src/Pages/BuyCrypto/BuyCrypto.js
+++ src/Pages/BuyCrypto/BuyCrypto.js
@@ -1,22 +1,51 @@
-import styles from '../../Styles/Pages/BuyCrypto/BuyCrypto.module.css'
-import BuySell from './Buy&Sell/BuySell';
-import RecurringBuy from './RecurringBuy/RecurringBuy';
+import styles from "../../Styles/Pages/BuyCrypto/BuyCrypto.module.css";
+import BuySell from "./Buy&Sell/BuySell";
+import RecurringBuy from "./RecurringBuy/RecurringBuy";
+import Withdraw from "./Withdraw/Withdraw";
+import { useState } from "react";
 const BuyCrypto = () => {
-    return (
-        <div className={styles.pageContainer}>
-            <div className={styles.actionsContainer}>
-                <ul className={styles.actionMenu}>
-                    <li className={styles.active}>Buy&Sell</li>
-                    <li className={styles.inactive}>Recurring Buy</li>
-                    <li className={styles.inactive}>Withdraw</li>
-                </ul>
-            </div>
-           
-                {/* <BuySell/> */}
-                <RecurringBuy />
-           
+  const [activeTab, setActiveTab] = useState("Buy&Sell");
 
-        </div>
-    )
-}
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+  };
+
+  return (
+    <div className={styles.pageContainer}>
+      <div className={styles.actionsContainer}>
+        <ul className={styles.actionMenu}>
+          <li
+            className={
+              activeTab === "Buy&Sell" ? styles.active : styles.inactive
+            }
+            onClick={() => handleTabClick("Buy&Sell")}
+          >
+            Buy&Sell
+          </li>
+          <li
+            className={
+              activeTab === "RecurringBuy" ? styles.active : styles.inactive
+            }
+            onClick={() => handleTabClick("RecurringBuy")}
+          >
+            Recurring Buy
+          </li>
+          <li
+            className={
+              activeTab === "Withdraw" ? styles.active : styles.inactive
+            }
+            onClick={() => handleTabClick("Withdraw")}
+          >
+            Withdraw
+          </li>
+        </ul>
+      </div>
+      <div className={styles.contentContainer}>
+        {activeTab === "Buy&Sell" && <BuySell />}
+        {activeTab === "RecurringBuy" && <RecurringBuy />}
+        {activeTab === "Withdraw" && <Withdraw />}
+      </div>
+    </div>
+  );
+};
 export default BuyCrypto;
---
+++ src/Pages/BuyCrypto/Withdraw/Withdraw.js
@@ -0,0 +1,15 @@
+import styles from '../../../Styles/Pages/BuyCrypto/Withdraw/Withdraw.module.css';
+const Withdraw= () =>{
+    return (
+        <div className={styles.withdrawContainer}>
+            <h1>Withdraw Fiat</h1>
+            <div>
+                <div>
+                
+                </div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+export default Withdraw;
